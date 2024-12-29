cf2af0970fecd521e17dfc70b40ddc617301699a
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2024-12-28 22:03:24 -0500

    minor adjustment to blending to account for more then just black bgs

--- matrix-hallway.glsl
+++ matrix-hallway.glsl
@@ -10,6 +10,8 @@
 #define SPEED_MULTIPLIER 1.
 #define GREEN_ALPHA .33
 
+#define BLACK_BLEND_THRESHOLD .4
+
 #define R fract(1e2 * sin(p.x * 8. + p.y))
 
 void mainImage(out vec4 fragColor, vec2 fragCoord) {
@@ -31,9 +33,8 @@ void mainImage(out vec4 fragColor, vec2 fragCoord) {
     vec2 uv = fragCoord.xy / iResolution.xy;
   	vec4 terminalColor = texture(iChannel0, uv);
 
-    // Make a mask that is 1.0 where the terminal content is not black
-    float mask = 1.2 - step(0.5, dot(terminalColor.rgb, vec3(1.0)));
-    vec3 blendedColor = mix(terminalColor.rgb * 1.2, col, mask);
+    float alpha = step(length(terminalColor.rgb), BLACK_BLEND_THRESHOLD);
+    vec3 blendedColor = mix(terminalColor.rgb * 1.2, col, alpha);
 
     fragColor = vec4(blendedColor, terminalColor.a);
 }
